e26d6c78d178707a7a984e24535f78eb36313eb3
qsc <qiu6288@gmail.com>
2015-07-04 20:58:35 +0800

    Initial Commit

---
+++ KQ1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KQ1
+//
+//  Created by 邱 士川 on 15/7/4.
+//  Copyright (c) 2015年 qsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KQ1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KQ1
+//
+//  Created by 邱 士川 on 15/7/4.
+//  Copyright (c) 2015年 qsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
