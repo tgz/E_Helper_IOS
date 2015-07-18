150faa2141415c014c892c86574f4e130cdef622
qsc <qiu6288@gmail.com>
2015-07-18 23:50:31 +0800

    new branch --- add cordite

--- KQ1/AppDelegate.h
+++ KQ1/AppDelegate.h
@@ -12,6 +12,15 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+/**添加CoreData的支持*/
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
 
 @end
 
