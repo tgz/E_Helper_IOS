b6bc36d063e39b274cfb99131d521f725e6ce26c
qsc <qiu6288@gmail.com>
2015-07-20 00:39:06 +0800

    add --- 给用户列表添加---移动顺序的方法，点击编辑的方法

--- KQ1/SwicthUserViewController.m
+++ KQ1/SwicthUserViewController.m
@@ -27,9 +27,19 @@
     [self.view setBackgroundColor:[UIColor whiteColor]];
     [self setTitle:@"用户管理"];
     
+    ///右侧的添加按钮
     UIBarButtonItem *rightBarItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
                                                                                  target:self action:@selector(goToLogin)];
     [self.navigationItem setRightBarButtonItem:rightBarItem];
+    
+    ///左侧的编辑按钮
+    UIBarButtonItem *leftBarItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemEdit
+                                                                                target:self
+                                                                                action:@selector(editUserList)];
+    [self.navigationItem setLeftBarButtonItem:leftBarItem];
+    
+    
+    
     
     /**添加通知--->切换页面**/
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -98,8 +108,36 @@
     
     return  cell;
 }
-
-
+/**实现了此方法，左滑会显示删除按钮*/
+- (void)tableView:(nonnull UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSLog(@"delete %dscetion %drow",indexPath.section,indexPath.row);
+        // userList removeObject:
+    //    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    
+        
+    }else if (editingStyle == UITableViewCellEditingStyleInsert){
+        NSLog(@"insert %d section %d row",indexPath.section,indexPath.row);
+    }
+    
+}
+/**只要实现这个方法在编辑状态右侧就有排序图标*/
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+//    KCContactGroup *sourceGroup =_contacts[sourceIndexPath.section];
+//    KCContact *sourceContact=sourceGroup.contacts[sourceIndexPath.row];
+//    KCContactGroup *destinationGroup =_contacts[destinationIndexPath.section];
+//    
+//    [sourceGroup.contacts removeObject:sourceContact];
+//    if(sourceGroup.contacts.count==0){
+//        [_contacts removeObject:sourceGroup];
+//        [tableView reloadData];
+//    }
+//    
+//    [destinationGroup.contacts insertObject:sourceContact atIndex:destinationIndexPath.row];
+    NSLog(@"remove %dSection %drow to %dSection %drow",
+          sourceIndexPath.section,sourceIndexPath.row,destinationIndexPath.section,destinationIndexPath.row);
+}
 
 #pragma mark - CustumDelegate
 
@@ -112,7 +150,14 @@
     [self presentViewController:loginVC animated:YES completion:nil];
     
 }
+
+- (void)editUserList {
+    [self.userList setEditing:!self.userList.isEditing animated:YES];
+}
 #pragma mark - private methods
+
+
+
 #pragma mark - Notification
 
 - (void)switchPage {
